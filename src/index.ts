551910b33009a4ccf55df7b905629aad87437de4
Jhonathan Rodriguez <jhonathanr2002@icloud.com>
2024-11-22 14:31:46 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,6 @@ export * from './services/basic-methods.service';
 export * from './services/typeorm-user-string.service';
 export * from './services/typeorm.service';
 
+export * from './types/lazy.type';
 export * from './types/property-name.type';
 export * from './types/message-args.type';
---
+++ src/types/lazy.type.ts
@@ -0,0 +1,1 @@
+export type LazyType<T> = Promise<T> | Partial<T>;
